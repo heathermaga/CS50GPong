da3fad1b68700b524cbcd9321a2adfc4fa23b047
Heather Maga <heatherrocks@gmail.com>
2018-09-22 00:41:08 -0400

    Comment Update
    updating the comment for further practicing development

--- main.lua
+++ main.lua
@@ -1,18 +1,11 @@
 --[[
-    GD50 2018
     Pong Remake
-    pong-0
-    "The Day-0 Update"
+    Game Development Practice for CS50G on EdX
+    https://github.com/games50/pong
+
     -- Main Program --
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
+    Sept. 22 2018 ~ See link above for orginals
+
 ]]
 
 WINDOW_WIDTH = 1280
